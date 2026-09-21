312fc9a033a690f7bb73e2d300a6624fd400582a
mythicalcosmos <360mcv@gmail.com>
2026-09-21 08:24:38 -0600

    gllllll :3

--- src/main/java/net/lucy/config/Hotkeys.java
+++ src/main/java/net/lucy/config/Hotkeys.java
@@ -6,9 +6,20 @@ import fi.dy.masa.malilib.config.options.ConfigHotkey;
 import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 
 public class Hotkeys {
-    public static final ConfigHotkey OPEN_GUI_MAIN_MENU                 = new ConfigHotkey("openGuiMainMenu",                   "M",    KeybindSettings.RELEASE_EXCLUSIVE, "Open the Litematica main menu");
+    public static final ConfigHotkey OPEN_GUI_MAIN_MENU = new ConfigHotkey("openGuiMainMenu","LEFT_SHIFT", KeybindSettings.RELEASE_EXCLUSIVE, "Opens the  main menu");
+    public static final ConfigHotkey OPEN_GUI_MATERIAL_LIST = new ConfigHotkey("openGuiMaterialList", "LEFT_SHIFT,L", "Opens the material List");
+    public static final ConfigHotkey OPEN_GUI_SELECTION_MANAGER = new ConfigHotkey("openGuiSelectionManager", "LEFT_SHIFT,S","Opens the litematic selection screen.");
+    public static final ConfigHotkey OPEN_GUI_SETTINGS = new ConfigHotkey("openGuiSettings", "LEFT_SHIFT,C", "Opens the settings menu");
+    public static final ConfigHotkey OPEN_GUI_BARITONE_SETTINGS = new ConfigHotkey("openGuiBaritoneSettings", "LEFT_SHIFT,D", "Opens the settings menu for Baritone");
+    public static final ConfigHotkey TOGGLE_INFO_OVERLAY_RENDERING = new ConfigHotkey("toggleInfoOverlayRendering", "", "Toggles the information for progress on the curent task with other similar info.");
+
 
     public static final List<ConfigHotkey> HOTKEY_LIST = ImmutableList.of(
-            OPEN_GUI_MAIN_MENU
+            OPEN_GUI_MAIN_MENU,
+            OPEN_GUI_MATERIAL_LIST,
+            OPEN_GUI_SELECTION_MANAGER,
+            OPEN_GUI_SETTINGS,
+            OPEN_GUI_BARITONE_SETTINGS,
+            TOGGLE_INFO_OVERLAY_RENDERING
     );
 }
